77813ee2e4bab85f467d3fa8effe57b12b12103a
Momo-gabriel <solyom.v.gabriel@gmail.com>
2025-12-17 14:20:10 +0200

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-# # def _on_quit(self):
-# #         """Properly exit the app."""
-# #         self.destroy()   # closes the Tk window and ends mainloop
-# #         os._exit(0)
-# import yfinance as yf
-# btc = yf.Ticker("BTC-USD")
-# print(btc.info.get("regularMarketPrice"))
-# verify_btc.py
